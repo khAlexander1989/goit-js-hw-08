e19998e1a241fe6a2330e90bae7a24226e729a17
Alexander Khardin <khporter1984@gmail.com>
2022-08-04 18:56:13 +0300

    create gallery via simpleLightBox library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('div.gallery'),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+refs.gallery.innerHTML = galleryMarkup;
+
+const modalOptions = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+const modal = createModal('.gallery a', modalOptions);
+
+//--------------FUNCTIONS------------------------
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"  />
+        </a>
+        `;
+    })
+    .join('');
+}
+
+function createModal(node, opts) {
+  return new SimpleLightbox(node, opts);
+}
