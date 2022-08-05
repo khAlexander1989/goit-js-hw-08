4df104f75ccc57f0e43203f24828e3fc66323b5f
Alexander Khardin <khporter1984@gmail.com>
2022-08-05 09:29:03 +0300

    create save current playback time feature

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,8 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 const refs = {
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+const refs = {
+  iframe: document.querySelector('#vimeo-player'),
+};
+
+const player = createPlayer(refs.iframe);
+setStartPlaybackTime(readPlaybackCurrentTime());
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+
+// -----------FUNCTIONS----------------------
+
+function createPlayer(element, opts = {}) {
+  return new VimeoPlayer(element, opts);
+}
+
+function createPlaybackCurrentTime(newCurrentPlaybackTime) {
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, newCurrentPlaybackTime);
+}
+
+function readPlaybackCurrentTime() {
+  return localStorage.getItem(VIDEOPLAYER_CURRENT_TIME) || 0;
+}
+
+function setStartPlaybackTime(newStartPlaybackTime) {
+  player.setCurrentTime(newStartPlaybackTime);
+}
+
+// -----------EVENT HANDLERS----------------------
+function onPlayerTimeUpdate({ seconds }) {
+  createPlaybackCurrentTime(seconds);
+}
